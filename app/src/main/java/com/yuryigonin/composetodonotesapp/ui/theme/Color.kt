01a3d84e634cb952f63c0d9380ac22d86d3e4609
worrum <yigoninwork@gmail.com>
2022-10-02 00:23:43 +0300

    Little UI changes

--- app/src/main/java/com/yuryigonin/composetodonotesapp/ui/theme/Color.kt
+++ app/src/main/java/com/yuryigonin/composetodonotesapp/ui/theme/Color.kt
@@ -1,13 +1,17 @@
 package com.yuryigonin.composetodonotesapp.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.Colors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val topAppBarColor = Color(0xFF051367)
 
 val LightGray = Color(0xFFFCFCFC)
 val MediumGray = Color(0xFF9C9C9C)
@@ -19,6 +23,9 @@ val MediumPriorityColor = Color(0xFFFFC114)
 val NonePriorityColor = MediumGray
 
 
+val ButtonLightThemeColor = Color(0xFF2D31FA)
+val ButtonDarkThemeColor = Color(0xFFfe8c05)
+
 val Colors.taskItemTextColor: Color
     @Composable
     get() = if (isLight) DarkGray else LightGray
@@ -29,7 +36,7 @@ val Colors.taskItemBackgroundColor: Color
 
 val Colors.fabBackgroundColor: Color
     @Composable
-    get() = if(isLight) Teal200 else Purple700
+    get() = if(isLight) ButtonLightThemeColor else ButtonDarkThemeColor
 
 val Colors.topAppBarContentColor: Color
     @Composable
@@ -37,4 +44,4 @@ val Colors.topAppBarContentColor: Color
 
 val Colors.topAppBarBackgroundColor: Color
     @Composable
-    get() = if(isLight) Purple500 else Color.Black
+    get() = if(isLight) topAppBarColor else Color.Black

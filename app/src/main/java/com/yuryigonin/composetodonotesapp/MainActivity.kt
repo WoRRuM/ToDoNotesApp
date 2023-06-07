1fde18ff766638f51d5922604e89fca1c0945df1
Yury Igonin <qeuiran@gmail.com>
2023-06-07 14:20:53 +0300

    Firebase Auth, Remote Server API

--- app/src/main/java/com/yuryigonin/composetodonotesapp/MainActivity.kt
+++ app/src/main/java/com/yuryigonin/composetodonotesapp/MainActivity.kt
@@ -1,23 +1,19 @@
 package com.yuryigonin.composetodonotesapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.yuryigonin.composetodonotesapp.navigation.SetupNavigation
 import com.yuryigonin.composetodonotesapp.ui.theme.ComposeToDoNotesAppTheme
 import com.yuryigonin.composetodonotesapp.ui.viewmodel.SharedViewModel
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @ExperimentalMaterialApi
 @AndroidEntryPoint
@@ -25,6 +21,8 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
     private val sharedViewModel: SharedViewModel by viewModels()
+    //private lateinit var mAuth: FirebaseAuth
+    //private lateinit var mAuthListener: FirebaseAuth.AuthStateListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +35,57 @@ class MainActivity : ComponentActivity() {
                 )
             }
         }
+        //mAuth = FirebaseAuth.getInstance();
+        //mAuth.firebaseAuthSettings.forceRecaptchaFlowForTesting(this)
+        // Test createAccount and signIn with hardcoded email and password
     }
+/*
+    private fun createAccount(email: String, password: String) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(
+                this
+            ) { task ->
+                if (task.isSuccessful) {
+                    // Sign up success, update UI with the signed-in user's information
+                    val user = mAuth.currentUser
+                } else {
+                    // If sign up fails, display a message to the user.
+                    Toast.makeText(
+                        this@MainActivity, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    private fun signIn(email: String, password: String) {
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(
+                this
+            ) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = mAuth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(
+                        this@MainActivity, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mAuth.addAuthStateListener(mAuthListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mAuth.removeAuthStateListener(mAuthListener)
+    }
+
+ */
 }
 
